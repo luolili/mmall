52fdbe9eb30a5f3050f74b27afc44528876e628b
Administrator <2084267015@qq.com>
2019-09-08 17:22:13 +0800

    pay

--- src/main/java/com/mmall/controller/portal/OrderController.java
+++ src/main/java/com/mmall/controller/portal/OrderController.java
@@ -1,23 +1,34 @@
 package com.mmall.controller.portal;
 
+import com.google.common.collect.Maps;
 import com.mmall.common.Const;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IOrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Iterator;
+import java.util.Map;
 
 @Controller
 @RequestMapping("order/")
 public class OrderController {
 
+    private Logger logger = LoggerFactory.getLogger(OrderController.class);
     @Autowired
     private IOrderService orderService;
+
+    @RequestMapping(value = "pay.do", method = RequestMethod.PUT)
+    @ResponseBody
     public ServerResponse pay(HttpSession session, Long orderNo, HttpServletRequest request) {
         User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
         if (currentUser == null) {
@@ -28,5 +39,28 @@ public class OrderController {
 
         return ServerResponse.createBySuccess(
                 orderService.pay(orderNo, currentUser.getId(), path));
+    }
+
+    @RequestMapping(value = "alipay_callback.do", method = RequestMethod.PUT)
+    @ResponseBody
+    public Object alipayCallback(HttpServletRequest request) {
+        Map<String, String> params = Maps.newHashMap();
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        Iterator<String> it = parameterMap.keySet().iterator();
+        for (; it.hasNext(); ) {
+            String name = (String) it.next();
+            String[] values = (String[]) parameterMap.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i] : values[i] + ",";
+            }
+            params.put(name, valueStr);
+        }
+        logger.info("支付宝callback: sign:{}, trade_status:{},参数：{}", params.get("sign"),
+                params.get("trade_status"), params.toString());
+        params.remove("sign_type");
+        return null;
+
+
     }
 }
