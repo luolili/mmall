36265600dab86fa28c26c05b9d247b0a2d78a209
Administrator <2084267015@qq.com>
2019-09-14 13:34:24 +0800

    redis

---
+++ src/main/java/com/mmall/controller/portal/UserSpringSessionController.java
@@ -0,0 +1,69 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/user/springsession")
+public class UserSpringSessionController {
+
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse resp) {
+        ServerResponse<User> response = userService.login(username, password);
+        if (response.isSuccess()) {
+            RedisPoolUtil.setEx(session.getId(), JsonUtil.obj2String(response.getData()),
+                    Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+            CookieUtil.writeLoginToken(resp, session.getId());
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "logout.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> loginout(HttpSession session, HttpServletResponse resp, HttpServletRequest req) {
+        //session.removeAttribute(Const.CURRENT_USER);
+        String loginToken = CookieUtil.readLoginToken(req);
+        CookieUtil.delLoginToken(req, resp);
+        RedisPoolUtil.del(loginToken);
+        return ServerResponse.createBySuccess();
+    }
+
+    @RequestMapping(value = "get_user_info.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpSession session, HttpServletRequest req) {
+        //User user = (User) session.getAttribute(Const.CURRENT_USER);
+        String loginToken = CookieUtil.readLoginToken(req);
+        if (StringUtils.isEmpty(loginToken)) {
+            return ServerResponse.createByErrorMessage("用户未登陆");
+        }
+        String userJsonStr = RedisPoolUtil.get(loginToken);
+        User user = JsonUtil.string2Obj(userJsonStr, User.class);
+
+        if (user != null) {
+            return ServerResponse.createBySuccess(user);
+        }
+        return ServerResponse.createByErrorMessage("用户未登陆");
+    }
+
+
+}
